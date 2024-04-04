81edc439308d7f0af72474eb3683acd3bcdd6386
uonr <dev@yuru.me>
2024-04-04 15:09:35 +0900

    feat: avatars

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Avatar, { AvatarProps } from 'boring-avatars';
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -25,8 +28,54 @@ export interface Env {
 	// MY_QUEUE: Queue;
 }
 
+const DEFAULT_COLORS = [
+	"#92A1C6",
+	"#146A7C",
+	"#F0AB3D",
+	"#C271B4",
+	"#C20D90",
+];
+
+const variantList: Array<AvatarProps['variant']> = ['marble', 'beam', 'pixel', 'sunset', 'ring', 'bauhaus'];
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		let cache = caches.default;
+		const cachedRes = await cache.match(request);
+		if (cachedRes) {
+			return cachedRes;
+		}
+		const url = new URL(request.url);
+		if (url.pathname === '/favicon.ico') {
+			return new Response(null, { status: 204 });
+		}
+		let name = '';
+		if (url.pathname.length > 1) {
+			name = url.pathname.substring(1, 128);
+		} else {
+			return new Response('Please provide a name', { status: 400 });
+		}
+
+		const digest = await crypto.subtle.digest(
+			{
+				name: 'SHA-256',
+			},
+			new TextEncoder().encode(name)
+		);
+		const number = new DataView(digest).getUint32(0);
+		const variant = variantList[number % variantList.length];
+		const avatar = renderToString(
+			React.createElement(Avatar, {
+				size: 200,
+				name,
+				variant,
+				colors: DEFAULT_COLORS,
+			}, null),
+		);
+		const res = new Response(avatar);
+		res.headers.set('Cache-Control', 'public, max-age=86400');
+		res.headers.set('Content-Type', "image/svg+xml");
+		await cache.put(request, res.clone());
+		return res;
 	},
 };
